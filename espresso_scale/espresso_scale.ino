0ac896628b8851b9d1711fb639691ee6ae9d6c43
ssk8 <stinkyskate@pm.me>
2023-06-18 10:14:07 -0500

    reimplement shot timer starter

--- espresso_scale/espresso_scale.ino
+++ espresso_scale/espresso_scale.ino
@@ -4,13 +4,14 @@
 
 Adafruit_DotStar dot = Adafruit_DotStar(1, 7, 8, DOTSTAR_BRG);
 
-#define SCALE_CALIBRATION 716
+#define SCALE_CALIBRATION 776
 
 #define LOADCELL_DOUT_PIN 3
 #define LOADCELL_SCK_PIN 4
 
 
 unsigned long timerStart = 0;
+bool preTime = true;
 
 HX711 scale;
 U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
@@ -26,13 +27,18 @@ void setup() {
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(SCALE_CALIBRATION);
   scale.tare(20);
-  
+
 }
 
 void loop() {
-    float weight = (scale.get_units(20));
+    float weight = (scale.get_units(5));
     if ((weight>-0.2) and (weight<0.2)) weight=0;
-
+    else {
+      if (preTime) {
+        preTime = false;
+        timerStart = millis();
+      }
+    }
     unsigned short timer = (millis() - timerStart)/1000;
 
     display.setFont(u8g2_font_helvB24_tf); 
